dcfd36ffb2cdbe71254468ae955a017cfdc01466
santhosh6194 <santhosh6194@gmail.com>
2022-01-17 00:06:48 +0530

    m30s-common: add samsung doze
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>

--- common.mk
+++ common.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libdrm.vendor:64
 
+# DOZE
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor:32 \
